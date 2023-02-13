9cdf5bb66697b81a2826e32c829981d5be86a7d7
ALPINISTLAR <niveam424@gmail.com>
2023-02-13 15:00:40 +0200

    add 'copy link to clipboard' function

--- js/main.js
+++ js/main.js
@@ -14,14 +14,16 @@ if (elUrlShortener) {
   elUrlShortener.addEventListener('click', function(evt) {
     if (evt.target.matches('.js-copy-short-link-button')) {
       // * Change button text * \\
-evt.target.textContent = 'Copied!';
-// * Change button bg color * \\
-evt.target.classList.add('url-shortener__copy-button--copied');
-// * Reset button text and class after 1 second * \\
-setTimeout(function () {
-evt.target.textContent = 'Copy';
-evt.target.classList.remove('url-shortener__copy-button--copied');
-}, 1000);
+      evt.target.textContent = 'Copied!';
+      // * Change button bg color * \\
+      evt.target.classList.add('url-shortener__copy-button--copied');
+      // * Copy short link to clipboard * \\
+      navigator.clipboard.writeText(evt.target.previousElementSibling.textContent);
+      // * Reset button text and class after 1 second * \\
+      setTimeout(function () {
+        evt.target.textContent = 'Copy';
+        evt.target.classList.remove('url-shortener__copy-button--copied');
+      }, 1000);
     }
   });
 }
